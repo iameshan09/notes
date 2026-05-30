d279d4c9edc81becf367a57426101c3cf0f797e0
iameshan09 <iameshan09@gmail.com>
2026-05-30 19:25:37 +0530

    Adding a billing API

--- infra/api.ts
+++ infra/api.ts
@@ -1,11 +1,11 @@
-import { table } from "./storage";
+import { table, secret } from "./storage";
 
 // Create the API
 export const api = new sst.aws.ApiGatewayV2("Api", {
   transform: {
     route: {
       handler: {
-        link: [table],
+        link: [table, secret],
       },
       args: {
         auth: { iam: true }
@@ -23,3 +23,5 @@ api.route("GET /notes", "packages/functions/src/list.main");
 api.route("PUT /notes/{id}", "packages/functions/src/update.main");
 
 api.route("DELETE /notes/{id}", "packages/functions/src/delete.main");
+
+api.route("POST /billing", "packages/functions/src/billing.main");
